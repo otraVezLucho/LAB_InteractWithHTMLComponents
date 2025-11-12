60ccd8625b5f4e91b059f741bc0f42d307f3787c
luisvillada <otrodeluisvillada@gmail.com>
2025-11-11 22:08:00 -0500

    Funciones de agregar valores creadas

--- Carrito_de_Compras/carrito.js
+++ Carrito_de_Compras/carrito.js
@@ -1,5 +1,55 @@
-class producto{
-    constructor(nombre, cantidad, precio){
+
+/*
+Implementar un sistema simple de carrito de compras que actualice cantidades y precios.
+1. Crea 3 productos con nombre y precio (usa elementos <span> para mostrar los precios).
+2. Para cada producto, agrega un botón "Agregar al carrito" con evento onclick que:
+•Actualice un contador de items totales (<span id="total-items">0</span>)
+•Sume el precio al total acumulado (<span id="total-precio">0</span>)
+3. Funcionalidades adicionales:
+•Muestra un alert() con el mensaje "¡Envío gratis aplicado!" cuando el total supere los $50.
+•Implementa un console.log() que liste los productos agregados al carrito.
+4. Agrega un botón "Vaciar carrito" que:
+•Restablezca todos los contadores a 0
+•Muestre un alert() con el mensaje "Carrito vaciado"
+*/
+
+
+
+let contadorp1 = 0;
+let contadorp2 = 0;
+let contadorp3 = 0;
+
+const preciop1 = 28;
+const preciop2 = 22;
+const preciop3 = 30;
+
+document.getElementById("precioP1").innerHTML = preciop1;
+
+document.getElementById("precioP2").innerHTML = preciop2;
+
+document.getElementById("precioP3").innerHTML = preciop3;
+
+function agregarP1() {
+    contadorp1++;
+    let total = preciop1 * contadorp1;
+    document.getElementById("total-items").innerHTML = total;
+    document.getElementById("unidadesP1").innerHTML = contadorp1;
+}
+function agregarP2() {
+    contadorp2++;
+    let total = preciop2 * contadorp2;
+    document.getElementById("total-items").innerHTML = total;
+    document.getElementById("unidadesP2").innerHTML = contadorp2;
+}
+function agregarP3() {
+    contadorp3++;
+    let total = preciop3 * contadorp3;
+    document.getElementById("total-items").innerHTML = total;
+    document.getElementById("unidadesP3").innerHTML = contadorp3;
+}
+
+class producto {
+    constructor(nombre, cantidad, precio) {
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.precio = precio;
